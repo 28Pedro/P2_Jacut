e12b3608e47709b1096b8163222b3b7b5203ab26
PedroGomes <pcabralgomes28@gmail.com>
2026-05-26 15:45:44 -0300

    Fix: Multiplos testes do Easyacept rodando

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import easyaccept.EasyAccept;
 
 public class Main {
-    public static void main(String[] args) {
-        String[] args2 = {"br.ufal.ic.p2.jackut.Facade",
-                "tests/us1_1.txt", "tests/us1_2.txt",
-                "tests/us2_1.txt", "tests/us2_2.txt",
-                "tests/us3_1.txt", "tests/us3_2.txt",
-                "tests/us4_1.txt", "tests/us4_2.txt",
-        };
-        EasyAccept.main(args2);
-    }
+        public static void main(String[] args) {
+            EasyAccept.main(new String[] {"br.ufal.ic.p2.jackut.Facade", "tests/us1_1.txt"});
+            EasyAccept.main(new String[] {"br.ufal.ic.p2.jackut.Facade", "tests/us1_2.txt"});
+            EasyAccept.main(new String[] {"br.ufal.ic.p2.jackut.Facade", "tests/us2_1.txt"});
+            EasyAccept.main(new String[] {"br.ufal.ic.p2.jackut.Facade", "tests/us2_2.txt"});
+            EasyAccept.main(new String[] {"br.ufal.ic.p2.jackut.Facade", "tests/us3_1.txt"});
+            EasyAccept.main(new String[] {"br.ufal.ic.p2.jackut.Facade", "tests/us3_2.txt"});
+            EasyAccept.main(new String[] {"br.ufal.ic.p2.jackut.Facade", "tests/us4_1.txt"});
+            EasyAccept.main(new String[] {"br.ufal.ic.p2.jackut.Facade", "tests/us4_2.txt"});
+        }
 }
